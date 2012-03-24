e5c2ea731f26316959dd4e410852fd9aecc831c5
Simon Kusterer <simon@velite.de>
2012-03-24 10:37:11 +0100

    setup basic world

---
+++ public/js/application.js
@@ -0,0 +1,69 @@
+(function() {
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function(/* function */ callback, /* DOMElement */ element){
+              window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
+  var world;
+
+  var init = function() {
+    var b2Vec2 = Box2D.Common.Math.b2Vec2;
+    var b2BodyDef = Box2D.Dynamics.b2BodyDef;
+    var b2Body = Box2D.Dynamics.b2Body;
+    var b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+    var b2Fixture = Box2D.Dynamics.b2Fixture;
+    var b2World = Box2D.Dynamics.b2World;
+    var b2MassData = Box2D.Collision.Shapes.b2MassData;
+    var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+    var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+    var b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+
+    world = new b2World(new b2Vec2(0, 10), true);
+
+    var fixDef = new b2FixtureDef;
+    fixDef.density = 1.0;
+    fixDef.friction = 0.5;
+    fixDef.restitution = 0.2;
+
+    var bodyDef = new b2BodyDef;
+
+    //create ground
+    bodyDef.type = b2Body.b2_staticBody;
+    bodyDef.position.x = 9;
+    bodyDef.position.y = 13;
+    fixDef.shape = new b2PolygonShape;
+    fixDef.shape.SetAsBox(10, 0.5);
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+    //setup debug draw
+    var debugDraw = new b2DebugDraw();
+    debugDraw.SetSprite(document.getElementById("canvas").getContext("2d"));
+    debugDraw.SetDrawScale(30.0);
+    //debugDraw.SetFillAlpha(1);
+    //debugDraw.SetLineThickness(1.0);
+    debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+    world.SetDebugDraw(debugDraw);
+
+    window.requestAnimFrame(update);
+  };
+
+  function update() {
+    world.Step(
+         1 / 60   //frame-rate
+      ,  10       //velocity iterations
+      ,  10       //position iterations
+    );
+    world.DrawDebugData();
+    world.ClearForces();
+   };
+
+  window.addEventListener('load', function(e) {
+    init();
+  });
+})();
